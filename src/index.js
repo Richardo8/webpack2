12fd84b232529e3de2e0df68c8aa6292975fde5e
LS <manunited_22@sina.com>
2017-07-18 16:03:16 +0800

    清理

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Rooney from './Rooney.jpg';
+import printMe from './print'
 
 function component(){
     var element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    var myIcon = new Image();
-    myIcon.src = Rooney;
-    element.appendChild(myIcon)
     return element;
 }
 
